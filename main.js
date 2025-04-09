b6de7f07aaae3e61f1d53bf79a34504f8e57586a
Igor Zabotin <igrzbtn@gmail.com>
2025-04-09 23:12:35 +0500

    header - optimized

---
+++ main.js
@@ -0,0 +1,29 @@
+// For header
+
+const headerLinks = document.querySelector(".nav-bar__links");
+const showHeaderLinks = () => headerLinks.classList.add("nav-bar__links--active");
+const hideHeaderLinks = () => headerLinks.classList.remove("nav-bar__links--active");
+
+const headerContent = document.querySelector(".header__content");
+const sliderCircles = document.querySelector(".slider__indicator").children;        
+const bgImages = ["./assets/img/dark-bg-1.webp", "./assets/img/dark-bg-2.webp", "./assets/img/dark-bg-3.webp"];
+
+(function autoSlide() {
+    const slidingFrequency = 3000;
+    let i = 0;
+
+    setInterval(() => {
+        headerContent.style.backgroundImage = `url(${bgImages[i]})`;
+
+        for (let circle of sliderCircles) {
+            circle.classList.remove("circle--chosen");
+        }
+
+        sliderCircles[i].classList.add("circle--chosen");
+
+        if (i == 2)
+            i = 0;
+        else
+            i++;
+    }, slidingFrequency);
+})();
